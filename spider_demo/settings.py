d8108881897e7c7914ce8cb11df5ffc30bdea249
chenzhilei <chenzl@bbtree.com>
2019-02-10 19:54:57 +0800

    add douban

--- spider_demo/settings.py
+++ spider_demo/settings.py
@@ -27,7 +27,7 @@ CONCURRENT_REQUESTS = 16
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 0.2  # s
+DOWNLOAD_DELAY = 1  # s
 DOWNLOAD_TIMEOUT = 15
 RETRY_ENABLED = False
 # The download delay setting will honor only one of:
